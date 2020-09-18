9f32ce584631e5be5d679fa97479dbd31eba8b11
Batuhan Gökçe <batuhan.gokce@intern.com>
2020-09-18 17:10:00 +0300

    added models

---
+++ src/main/java/com/bgokce/coursemanagementwebapplication/model/Assistant.java
@@ -0,0 +1,16 @@
+package com.bgokce.coursemanagementwebapplication.model;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.SequenceGenerator;
+
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@Setter
+@Getter
+@SequenceGenerator(name = "idgen", sequenceName = "student_seq")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Assistant extends PersonEntity {
+}
