9bd62f67e35a9a892e7f86b6e2a1b9f32b2d4ce7
Batuhan Gökçe <batuhan.gokce@intern.com>
2020-09-28 16:48:55 +0300

    edit login page

--- src/main/webapp/src/App.js
+++ src/main/webapp/src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import {Redirect, Route} from "react-router-dom";
+import Home from "./pages/Home";
 
 
 function App() {
   return (
-    <div/>
+      <div className="App-intro">
+        <Route path='/home' exact component={Home} />
+        <Route path='/next' component={Home} />
+        <Route path='/last' component={Home} />
+        <Redirect to='/home' />
+      </div>
   );
 }
 
